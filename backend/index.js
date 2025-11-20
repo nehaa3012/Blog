4eb450f4d7cf1efdc102789dbaa36e9c14b87a6c
nehaa3012 <nehach782@gmail.com>
2025-11-20 14:22:29 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -16,14 +16,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "https://blog-frontend-a8ze.onrender.com",
+  origin: "https://fascinating-melba-f9ff0c.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
 // This automatically handles OPTIONS → No need to define manually
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://blog-frontend-a8ze.onrender.com");
+  res.header("Access-Control-Allow-Origin", "https://fascinating-melba-f9ff0c.netlify.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
